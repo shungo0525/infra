d62c38c545491a8a72e45d566c4e7282378b779e
shungo0525 <shungo0525@gmail.com>
2021-04-03 16:22:09 +0900

    サブネット、インターネットゲートウェイを作成

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,3 +9,33 @@ resource "aws_vpc" "main" {
     Name = "test-vpc"
   }
 }
+
+resource "aws_subnet" "public_1a" {
+  # 作成したVPCを参照し、そのVPC内にSubnetを立てる
+  vpc_id = aws_vpc.main.id
+  # Subnetを作成するAZ
+  availability_zone = "ap-northeast-1a"
+  cidr_block        = "10.0.1.0/24"
+  tags = {
+    Name = "test-subnet-public-1a"
+  }
+}
+
+resource "aws_subnet" "public_1c" {
+  # 作成したVPCを参照し、そのVPC内にSubnetを立てる
+  vpc_id = aws_vpc.main.id
+  # Subnetを作成するAZ
+  availability_zone = "ap-northeast-1c"
+  cidr_block        = "10.0.2.0/24"
+  tags = {
+    Name = "test-subnet-public-1c"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "test"
+  }
+}

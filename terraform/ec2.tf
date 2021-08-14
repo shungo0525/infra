d56710ea100a156fabf34350bf61b904feb8266a
shungo0525 <shungo0525@gmail.com>
2021-08-14 11:31:52 +0900

    ec2切り出し

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,69 +1,3 @@
-provider "aws" {
-  region = "ap-northeast-1"
-}
-
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "test-vpc"
-  }
-}
-
-resource "aws_subnet" "public_1a" {
-  # 作成したVPCを参照し、そのVPC内にSubnetを立てる
-  vpc_id = aws_vpc.main.id
-  # Subnetを作成するAZ
-  availability_zone = "ap-northeast-1a"
-  cidr_block        = "10.0.1.0/24"
-  tags = {
-    Name = "test-subnet-public-1a"
-  }
-}
-
-resource "aws_subnet" "public_1c" {
-  # 作成したVPCを参照し、そのVPC内にSubnetを立てる
-  vpc_id = aws_vpc.main.id
-  # Subnetを作成するAZ
-  availability_zone = "ap-northeast-1c"
-  cidr_block        = "10.0.2.0/24"
-  tags = {
-    Name = "test-subnet-public-1c"
-  }
-}
-
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "test-internet-gateway"
-  }
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "test-route-table"
-  }
-}
-
-resource "aws_route" "public" {
-  destination_cidr_block = "0.0.0.0/0"
-  route_table_id         = aws_route_table.public.id
-  gateway_id             = aws_internet_gateway.main.id
-}
-
-resource "aws_route_table_association" "public_1a" {
-  subnet_id      = aws_subnet.public_1a.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route_table_association" "public_1c" {
-  subnet_id      = aws_subnet.public_1c.id
-  route_table_id = aws_route_table.public.id
-}
-
 data "aws_ssm_parameter" "amzn2_ami" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
@@ -71,36 +5,36 @@ data "aws_ssm_parameter" "amzn2_ami" {
 resource "aws_instance" "a" {
   ami                         = data.aws_ssm_parameter.amzn2_ami.value
   instance_type               = "t3.nano"
-  key_name                    = "test-hoge" // AWSコンソールで生成したキーペアの名前
+  key_name                    = "${var.app_name}-key-pair" // AWSコンソールで生成したキーペアの名前
   subnet_id                   = aws_subnet.public_1a.id
   security_groups             = [aws_security_group.test.id]
   associate_public_ip_address = true
 
   tags = {
-    Name = "test-ec2-a"
+    Name = "${var.app_name}-ec2-a-${var.environment}"
   }
 }
 
 resource "aws_instance" "c" {
   ami                         = data.aws_ssm_parameter.amzn2_ami.value
   instance_type               = "t3.nano"
-  key_name                    = "test-hoge" // AWSコンソールで生成したキーペアの名前
+  key_name                    = "${var.app_name}-key-pair" // AWSコンソールで生成したキーペアの名前
   subnet_id                   = aws_subnet.public_1c.id
   security_groups             = [aws_security_group.test.id]
   associate_public_ip_address = true
 
   tags = {
-    Name = "test-ec2-c"
+    Name = "${var.app_name}-ec2-c-${var.environment}"
   }
 }
 
 // security-group
 resource "aws_security_group" "test" {
-  name        = "test-security-group"
+  name        = "${var.app_name}-security-group-${var.environment}"
   vpc_id      = aws_vpc.main.id
 
   tags = {
-    Name = "test-security-group"
+    Name = "${var.app_name}-security-group-${var.environment}"
   }
 }
 
@@ -139,7 +73,7 @@ resource "aws_eip" "a" {
   vpc = true
 
   tags = {
-    Name = "test-eip-a"
+    Name = "${var.app_name}-eip-a-${var.environment}"
   }
 }
 
@@ -148,6 +82,6 @@ resource "aws_eip" "c" {
   vpc = true
 
   tags = {
-    Name = "test-eip-c"
+    Name = "${var.app_name}-eip-c-${var.environment}"
   }
 }
